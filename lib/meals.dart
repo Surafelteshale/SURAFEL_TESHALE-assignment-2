12066bb9875429c9ce3fa528de4bc13908c2bac3
Surafelteshale <you@example.com>
2023-02-09 04:33:10 -0600

    update

--- lib/meals.dart
+++ lib/meals.dart
@@ -48,11 +48,22 @@ class meals extends StatelessWidget {
                                   // , height: 100, width: 100,
                                   // ),
                                   SizedBox(height: 100, width: 150),
-                                  const Text('Beyaynet',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Color(0xfffac585)
-                                      )
+                                  Column(
+                                    children: const [
+                                      SizedBox(height: 35,),
+                                      Text('Beyaynet',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Color(0xfffac585)
+                                          )
+                                      ),
+                                      Text('200 birr',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Color(0xfffac585)
+                                          )
+                                      ),
+                                    ],
                                   ),
                                   Container(
                                     decoration: BoxDecoration(
@@ -72,16 +83,15 @@ class meals extends StatelessWidget {
                     ),
                       ),
                       Container(
-                        padding: EdgeInsets.only(left: 20),
+                        padding: EdgeInsets.only(left: 70,top: 40),
                         child: Image.asset("images/image1.png",
-                          height: 300,
-                          width: 300,
+                          height: 200,
+                          width: 200,
                         ),
                       ),
                     ]
                   ),
                 ),
-              SizedBox(height: 2),
               Container(
                 child: Stack(
                     children:[
@@ -118,11 +128,22 @@ class meals extends StatelessWidget {
                                     // , height: 100, width: 100,
                                     // ),
                                     SizedBox(height: 100, width: 150),
-                                    const Text('Kitfo',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Color(0xfffac585)
-                                        )
+                                    Column(
+                                      children: const [
+                                        SizedBox(height: 35,),
+                                        Text('Kitfo',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xfffac585)
+                                            )
+                                        ),
+                                        Text('1000 birr',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xfffac585)
+                                            )
+                                        ),
+                                      ],
                                     ),
                                     Container(
                                         decoration: BoxDecoration(
@@ -187,11 +208,28 @@ class meals extends StatelessWidget {
                                     // , height: 100, width: 100,
                                     // ),
                                     SizedBox(height: 100, width: 150),
-                                    const Text('Tere Sega',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Color(0xfffac585)
-                                        )
+                                    Column(
+                                      children: const [
+                                        SizedBox(height: 35,width: 20,),
+                                        Text('Tere Sega',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xfffac585)
+                                            )
+                                        ),
+                                        Text('1300 birr',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xfffac585)
+                                            )
+                                        ),
+                                        Text('1 k.g',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xfffac585)
+                                            )
+                                        ),
+                                      ],
                                     ),
                                     Container(
                                         decoration: BoxDecoration(
@@ -211,81 +249,15 @@ class meals extends StatelessWidget {
                         ),
                       ),
                       Container(
-                        padding: EdgeInsets.only(left: 20),
+                        padding: EdgeInsets.only(left: 50,top: 1),
                         child: Image.asset("images/image3.png",
                           height: 300,
                           width: 300,
                         ),
                       ),
-
                     ]
                 ),
               ),
-              //  Container(
-              //    //height: 1000,
-              //   child: Stack(
-              //       children:[
-              //         Center(
-              //           child: Container(
-              //             //margin: EdgeInsets.only(top: 100),
-              //             //padding: EdgeInsets.only(top: 25),
-              //             height: 100,
-              //             width: 350,
-              //             decoration: BoxDecoration(
-              //                 borderRadius: BorderRadius.circular(10),
-              //                 boxShadow: [
-              //                   BoxShadow(
-              //                       color: Colors.grey.withOpacity(0.5),
-              //                       spreadRadius: 1,
-              //                       blurRadius: 8
-              //                   ),
-              //                 ],
-              //                 gradient: const LinearGradient(
-              //                   begin: Alignment.topRight,
-              //                   end: Alignment.bottomLeft,
-              //                   colors: [
-              //                     Color(0xff165967),
-              //                     Color(0xff23aea6),
-              //                   ],
-              //                 )
-              //             ),
-              //             child: Center(
-              //               child: Container(
-              //                   child: Row(
-              //                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //                     children: [
-              //                       // Image.asset("images/image$i.png"
-              //                       // , height: 100, width: 100,
-              //                       // ),
-              //                       SizedBox(height: 100, width: 150),
-              //                       Text('Title'),
-              //                       Container(
-              //                           decoration: BoxDecoration(
-              //                             borderRadius: BorderRadius.circular(5),
-              //                             color: Colors.white,
-              //                           ),
-              //                           height: 25,
-              //                           width: 25,
-              //
-              //                           child: Icon(Icons.favorite)
-              //
-              //                       )
-              //                     ],
-              //                   )
-              //               ),
-              //             ),
-              //           ),
-              //         ),
-              //         Container(
-              //           padding: EdgeInsets.only(left: 20, top: 10),
-              //           child: Image.asset("images/image2.png",
-              //             height: 300,
-              //             width: 300,
-              //           ),
-              //         ),
-              //       ]
-              //   ),
-              // )
             ],
           ),
         ),
